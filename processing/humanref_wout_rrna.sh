ec1b66ab62f94bdd983c09a88fc745b0eec343ed
karina <karin.isaev@gmail.com>
2022-04-20 12:10:24 -0400

    add bbmap functions

--- processing/humanref_wout_rrna.sh
+++ processing/humanref_wout_rrna.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 #humanref_wout_rrna.sh
+#mask the rRNA sequence in the human genome 
+
+#where to save masked reference 
+output=/home/keren/DATA/RNAseq/human
 
 #If you know the 16S sequences (or those of a close relative), you can align them to the genome to produce a sam file. For example:
 mapPacBio=/home/keren/ANALYSIS/bbmap/mapPacBio.sh
@@ -9,10 +13,16 @@ bbmask=/home/keren/ANALYSIS/bbmap/bbmask.sh
 ribo=/home/keren/DATA/rRNA_genbank_U13369_1.fasta
 human=/home/keren/DATA/GRCh38_latest_genomic.fna.gz
 
-$mapPacBio ref=$human in=$ribo out=mapped.sam ambig=all maxindel=20 
+$mapPacBio ref=$human in=$ribo out=$output/mapped.sam ambig=all maxindel=20 nodisk usemodulo
 
 #Then you can run BBMask:
 
-$bbmask in=$human out=masked.fasta sam=mapped.sam masklowentropy=false maskrepeats=false
+$bbmask in=$human out=$output/human_rRNA_masked.fasta sam=$output/mapped.sam masklowentropy=false maskrepeats=false
 
-#torun nohup ./annotate_vcfs.sh & #(will keep running in background)
+#remove the mapped.sam file as we don't need it
+rm $output/mapped.sam
+
+echo DONE
+
+#torun 
+#nohup ./humanref_wout_rrna.sh & #(will keep running in background)
