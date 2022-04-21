b7a3fa6d6a76f77660dd98cb7c305d7e40e98f80
karina <karin.isaev@gmail.com>
2022-04-21 11:00:27 -0400

    update human reference index

--- processing/make_human_index.sh
+++ processing/make_human_index.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-human=/home/keren/DATA/GRCh38_latest_genomic.fna.gz
+human=/home/keren/DATA/human_ref/GRCh38_latest_genomic.fna.gz
 
 echo Indexing the human reference genome GRCh38_latest_genomic
 
 bwa index ${human} #just do once 
+
+#nohup ./annotate_vcfs.sh & #(will keep running in background)
 
