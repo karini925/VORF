8ad122556c8d345e38a86918979dbe586da7952e
karina <karin.isaev@gmail.com>
2022-05-04 11:47:01 -0400

    update vorf

--- assemblers/run_blast.sh
+++ assemblers/run_blast.sh
@@ -14,6 +14,6 @@ cd $id
 #for each fasta file run tblastn
 for file in *.faa ; do
 	echo "processing file=$file"
-	tblastn -db nr -query $file  -out ${file}_output_blast.txt -outfmt 6 -remote 
+	tblastn -db nr -query $file  -out ${file}_output_blast.txt -max_target_seqs 5 -outfmt '6 qseqid sseqid evalue bitscore sgi sacc staxids sscinames scomnames stitle' -remote 
 done
 
--- assemblers/run_blastn_iva.sh
+++ assemblers/run_blastn_iva.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo blast results from VORF:
+echo blast results from IVA:
 
 #file name (input into the script is the original bam files)
 cd /home/keren/DATA/RNAseq
@@ -12,5 +12,5 @@ cd /home/keren/DATA/RNAseq/postQCrnaseq/aligned
 cd ${id}_iva
 
 #for each fasta file run tblastn
-blastn -db nr -query contigs.fasta -out ${id}_output_blast.txt -outfmt 6 -remote 
+blastn -db nr -query test.fasta -out ${id}_output_blast.txt -max_target_seqs 5 -outfmt '6 qseqid sseqid evalue bitscore sgi sacc staxids sscinames scomnames stitle' -remote 
 
